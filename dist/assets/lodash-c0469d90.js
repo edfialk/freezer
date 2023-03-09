b0a49eedd8ef9e1da2f9e1119f690b6e6fb92ae6
edfialk <edfialk@gmail.com>
2023-03-08 20:10:29 -0800

    trying to fix base

--- dist/assets/lodash-c0469d90.js
+++ dist/assets/lodash-c0469d90.js
@@ -1,4 +1,4 @@
-import{m as ne}from"./index-1015b875.js";var Ri={},rp={get exports(){return Ri},set exports(Ot){Ri=Ot}};/**
+import{m as ne}from"./index-2949f283.js";var Ri={},rp={get exports(){return Ri},set exports(Ot){Ri=Ot}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
