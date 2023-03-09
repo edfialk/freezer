4e1732e9e08a1cddf74c91fa9e0d31a491b8b8ec
edfialk <edfialk@gmail.com>
2023-03-08 20:02:34 -0800

    fix dist base

--- assets/lodash-204a2b5c.js
+++ assets/lodash-204a2b5c.js
@@ -1,4 +1,4 @@
-import{m as ne}from"./index-00ce1168.js";var Ri={},rp={get exports(){return Ri},set exports(Ot){Ri=Ot}};/**
+import{m as ne}from"./index-1015b875.js";var Ri={},rp={get exports(){return Ri},set exports(Ot){Ri=Ot}};/**
  * @license
  * Lodash <https://lodash.com/>
  * Copyright OpenJS Foundation and other contributors <https://openjsf.org/>
